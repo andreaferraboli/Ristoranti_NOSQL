6e7d37589c78b260171bcfd99eca662aacb47993
LucaLombardo03 <lucalombardo03@gmail.com>
2022-04-20 18:42:52 +0200

    addRestaurant.html and addRestaurant.js + fix file

---
+++ Realtime/addRestaurant.js
@@ -0,0 +1,52 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBZn1RMg1jaFSKZEUuHOZwLyDM2s_VrWTc",
+    authDomain: "prova-2881d.firebaseapp.com",
+    databaseURL: "https://prova-2881d-default-rtdb.firebaseio.com",
+    projectId: "prova-2881d",
+    storageBucket: "prova-2881d.appspot.com",
+    messagingSenderId: "430537388837",
+    appId: "1:430537388837:web:abeeff9164a271480ee0cc",
+    measurementId: "G-Y5KYS4M1F9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Set database variable
+var database = firebase.database()
+
+function save() {
+
+    var nome = document.getElementById('nome').value
+    var via = document.getElementById('via').value
+    var numero_civico = document.getElementById('numero_civico').value
+    var cap = document.getElementById('cap').value
+    var citta = document.getElementById('citta').value
+    var link = document.getElementById('link').value
+    var maps = document.getElementById('maps').value
+    var recensione = document.getElementById('recensione').value
+    var sito = document.getElementById('sito').value
+    var telefono = document.getElementById('telefono').value
+    var immagine = document.getElementById('immagine').value
+
+    if (nome == "" || via == "" || numero_civico == "" || cap == "" || citta == "" || link == "" || maps == "" || recensione == "" || sito == "" ||
+        telefono == "" || immagine == "") {
+        alert("Compila tutti i campi");
+        return false;
+    }
+    database.ref('users/' + nome).set({
+        nome: nome,
+        via: via,
+        numero_civico: numero_civico,
+        cap: cap,
+        citta: citta,
+        link: link,
+        maps: maps,
+        recensione: recensione,
+        sito: sito,
+        telefono: telefono,
+        immagine: immagine
+    })
+
+    alert('Ristorante aggiunto')
+}
