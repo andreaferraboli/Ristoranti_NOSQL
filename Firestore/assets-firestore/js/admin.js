07e3d4d27f731f58ec7fb5191a19c7321e2bdbfa
ROMA030 <dani21roby@gmail.com>
2022-04-25 15:41:34 +0200

    Added admin to firestore, need to adjust modify and eliminate

--- Firestore/assets-firestore/js/admin.js
+++ Firestore/assets-firestore/js/admin.js
@@ -1,81 +1,80 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js";
-import {getDatabase, onValue, query, ref, remove} from "https://www.gstatic.com/firebasejs/9.6.9/firebase-database.js";
-// import {currentId} from "./updateRestaurant.js";
+import {getFirestore} from "https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
 
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAyK1ThxpKUGHEKtlg4W3EJZEBnPHjH-Ic",
-    authDomain: "ristoranti-31ef8.firebaseapp.com",
-    projectId: "ristoranti-31ef8",
-    storageBucket: "ristoranti-31ef8.appspot.com",
-    messagingSenderId: "236248778413",
-    appId: "1:236248778413:web:0b004e239a04c74793a824",
-    databaseURL: "https://ristoranti-31ef8-default-rtdb.firebaseio.com",
+    apiKey: "AIzaSyC6_tMUB9BKh9Fi_WoYZYjCT9Xlgf8q5CY",
+    authDomain: "ristorantilombardi.firebaseapp.com",
+    projectId: "ristorantilombardi",
+    storageBucket: "ristorantilombardi.appspot.com",
+    messagingSenderId: "476667917046",
+    appId: "1:476667917046:web:8d01a4cd11f6b59b9432d7",
+    measurementId: "G-2YMQP9WKN0"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+self.firebase = getFirestore(app);
 
-const db = getDatabase(app);
 
-console.log(db);
-// recupera e stampa valore della chiave 'testo'
-const output1 = document.getElementById("restaurants");
-// var dbRef = ref(db, "/Ristoranti/");
-let dbRef = query(ref(db, '/Ristoranti/'));
-onValue(dbRef, (snap) => {
-    const obj = JSON.parse(JSON.stringify(snap.val(), null, 2));
-    console.log(obj);
-    console.log(Object.keys(obj));
-    for (const i of Object.keys(obj)) {
-        if (obj[i] != null) {
-            let stringBuilder = '<section class="restaurant-section section-bg">';
-            stringBuilder += '<div class="card" >'
-            stringBuilder += '<div class="row no-gutters">'
-            stringBuilder += '<div class="col-sm-5 cardImg">'
-            stringBuilder += '<img class="Img" src="' + obj[i].Img + '" alt="Suresh Dasari Card">'
-            stringBuilder += '</div>'
-            stringBuilder += '<div class="col-sm-7 card-div">'
-            stringBuilder += '<div class="card-body">'
-            stringBuilder += '<div class="card-item"><h5 class="card-title">' + obj[i].Nome + '</h5></div>'
-            stringBuilder += '<div class="card-item"><p class="card-text">Valutazione: ' + obj[i].Recensione + '<span class="star" >&starf;</span></p></div>'
-            stringBuilder += '<div class="card-item">'
-            stringBuilder += '<div class="col-sm-6">'
-            stringBuilder += '<div class="card-item"><h5 class="card-title">' + obj[i].Posizione.via + "," + obj[i].Posizione["numero_civico"] + "," + obj[i].Posizione.cap + ", " + obj[i].Posizione.città + '</h5></div>'
-            stringBuilder += '</div>'
-            stringBuilder += '<div class="col-sm-6">'
-            stringBuilder += '<iframe src="' + obj[i].Posizione.mappa + '" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
-            stringBuilder += '</div>'
-            stringBuilder += '</div>'
-            stringBuilder += '<div class="card-item">'
-            stringBuilder += '<button  id="button_delete_' + i + '" class="btn-delete" data-toggle="tooltip" data-placement="top" title="elimina">Elimina</button>'
-            stringBuilder += '<button id="button_modify_' + i + '" class="btn-modify" data-toggle="tooltip" data-placement="top" title="modifica">Modifica</button>'
-            stringBuilder += '</div>'
-            stringBuilder += '</div>'
-            stringBuilder += '</div>'
-            stringBuilder += '</div>'
-            stringBuilder += '</section>'
-            // stringBuilder += '<script>'
-            // stringBuilder += 'function eliminaRistorante(event) {'
-            // stringBuilder += ' event.stopPropagation();'
-            // stringBuilder += ' event.target.id'
-            // stringBuilder += '            alert("elimina: " + id);'
-            // stringBuilder += '            let restaurantRef = ref(db, "Ristoranti" + "/" + "0");'
-            //
-            // // remove review
-            // stringBuilder += '            remove(restaurantRef);'
-            // stringBuilder += '        }'
-            // stringBuilder += ''
-            // stringBuilder += '</script>'
-            output1.innerHTML += stringBuilder;
-            // document.getElementById("button_"+i).addEventListener("click",event =>{
-            //     console.log("ristorante");
-            //     let restaurantRef = ref(db, "Ristoranti" + "/" + i);
-            //     // remove review
-            //     remove(restaurantRef);
-            //     alert("ristorante eliminato")
-            // })
+    import { doc, getDoc, getDocs, collection, query } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js";
+
+    console.log(firebase);
+
+    let collectionRef=collection(firebase, "ristoranti");
+    let docs=await getDocs(query(collectionRef));
+
+    let i=0;
+    docs.forEach(
+    (doc) =>{
+    i=parseInt(doc.id);
+
+    var Ristorante = doc.data();
+    console.log(Ristorante);
+    var Nome = Ristorante.informazioni.nome;
+    var Img = Ristorante.informazioni.immagine;
+    var Valutazione = Ristorante.informazioni.valutazione;
+    var Via = Ristorante.posizione.via;
+    var N_civico = Ristorante.posizione.n_civico;
+    var CAP = Ristorante.posizione.CAP;
+    var Città = Ristorante.posizione.città;
+    var Telefono = Ristorante.contatti.telefono;
+    var Sito_web = Ristorante.contatti.link;
+    var Mappa = Ristorante.contatti.mappa;
+    console.log(Nome);
+    console.log(Img);
+
+    var stringBuilder = '<section class="restaurant-section section-bg">';
+    stringBuilder += '<div class="card" >'
+    stringBuilder += '<div class="row no-gutters">'
+    stringBuilder += '<div class="col-sm-5 cardImg">'
+    stringBuilder += '<img class="Img" src="' + Img + '" alt="Suresh Dasari Card">'
+    stringBuilder += '</div>'
+    stringBuilder += '<div class="col-sm-7 card-div">'
+    stringBuilder += '<div class="card-body">'
+    stringBuilder += '<div class="card-item"><h5 class="card-title">' + Nome + '</h5></div>'
+    stringBuilder += '<div class="card-item"><p class="card-text">Valutazione: ' + Valutazione + '<span class="star" >&starf;</span></p></div>'
+    stringBuilder += '<div class="card-item">'
+    stringBuilder += '<div class="col-sm-6">'
+    stringBuilder += '<div class="card-item"><h5 class="card-title">' + Via + "," + N_civico + "," + CAP + ", " + Città + '</h5></div>'
+    stringBuilder += '</div>'
+    stringBuilder += '<div class="col-sm-6">'
+    stringBuilder += '<iframe src="' + Mappa + '" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+    stringBuilder += '</div>'
+    stringBuilder += '</div>'
+    stringBuilder += '<div class="card-item">'
+        stringBuilder += '<button  id="button_delete_' + Ristorante + '" class="btn-delete" data-toggle="tooltip" data-placement="top" title="elimina">Elimina</button>'
+        stringBuilder += '<button id="button_modify_' + Ristorante + '" class="btn-modify" data-toggle="tooltip" data-placement="top" title="modifica">Modifica</button>'
+    stringBuilder += '</div>'
+    stringBuilder += '</div>'
+    stringBuilder += '</div>'
+    stringBuilder += '</div>'
+    stringBuilder += '</section>'
+
+
+    document.getElementById("ristorante").innerHTML += stringBuilder;
+
             document.addEventListener('click', function (e) {
                 if (e.target && e.target.id === 'button_delete_' + i) {
                     let restaurantRef = ref(db, "Ristoranti" + "/" + i);
@@ -90,8 +89,6 @@ onValue(dbRef, (snap) => {
 
                     }
             });
-        }
-    }
 });
 
 
